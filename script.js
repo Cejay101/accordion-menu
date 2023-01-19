54c13976d938fb8a216f1c9c87ff70f05760878b
cejay101 <clintonjoyfimie@gmail.com>
2023-01-19 14:01:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function accordion(multiple = false) {
+  const accordion = document.querySelector(".accordion-menu");
+  const dropdownlink = accordion.querySelectorAll(".dropdownlink");
+
+  dropdownlink.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      const next = link.nextElementSibling;
+      next.style.display = next.style.display === "block" ? "none" : "block";
+      link.parentElement.classList.toggle("open");
+
+      if (!multiple) {
+        accordion.querySelectorAll(".submenuItems").forEach((submenu) => {
+          if (submenu !== next) {
+            submenu.style.display = "none";
+            submenu.parentElement.classList.remove("open");
+          }
+        });
+      }
+    });
+  });
+}
+accordion();
