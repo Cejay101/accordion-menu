b2513d9475e7929831ac5b5f8fa2d42f87904c69
cejay101 <clintonjoyfimie@gmail.com>
2023-01-22 06:20:02 +0100

    change

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function accordion(multiple = false) {
   const accordion = document.querySelector(".accordion-menu");
   const dropdownlink = accordion.querySelectorAll(".dropdownlink");
-
   dropdownlink.forEach((link) => {
     link.addEventListener("click", (e) => {
       const next = link.nextElementSibling;
       next.style.display = next.style.display === "block" ? "none" : "block";
       link.parentElement.classList.toggle("open");
-
       if (!multiple) {
         accordion.querySelectorAll(".submenuItems").forEach((submenu) => {
           if (submenu !== next) {
